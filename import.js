85b56004241d516d8affef408bb31abe93c4d963
Arun Kumar <stockerunknown@gmail.com>
2025-01-08 00:30:46 +0530

    Added in folder

--- import.js
+++
@@ -1,9 +0,0 @@
-
-
-const variable = require('./export.js');
-console.log(variable.first)
-console.log(variable.second)
-console.log(variable.third)
-
-
-// So here is we imported the a,b,c variablle from export.js
